9d1e8e41d50ed7d9c5b04841f78f2060bf09e0c3
joshuajong1 <90546610+joshuajong1@users.noreply.github.com>
2021-10-04 16:32:34 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,10 @@
+
+var vg_1 = "map_wk9.vg.json";
+vegaEmbed("#map", vg_1).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "heatmap.vg.json";
+vegaEmbed("#heatmap-cases", vg_2).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
